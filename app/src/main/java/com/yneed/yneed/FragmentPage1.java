c055cc918dfcfb46b033878a4df2fe59339c5f9f
WJ <wangjiosw@gmail.com>
2016-02-06 16:18:20 +0800

    照着网上打的还有问题
    附网址：http://www.it165.net/pro/html/201409/23088.html

---
+++ app/src/main/java/com/yneed/yneed/FragmentPage1.java
@@ -0,0 +1,19 @@
+package com.yneed.yneed;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by apple on 16/2/6.
+ */
+public class FragmentPage1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_0, null);
+    }
+}
