3911a5c6ecadd4589814382fb0e0ebe1e03cfb95
CXWZJUT <357668900@qq.com>
2016-02-09 20:23:56 +0800

    滚动图

--- app/src/main/java/com/yneed/yneed/Yneedapp.java
+++ app/src/main/java/com/yneed/yneed/Yneedapp.java
@@ -1,32 +1,28 @@
 package com.yneed.yneed;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import android.app.Activity;
-import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class Yneedapp extends Activity implements OnClickListener
-{
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class Yneedapp extends Activity implements OnClickListener {
 
     private ViewPager mViewPager;
     private PagerAdapter mAdapter;
@@ -41,10 +37,17 @@ public class Yneedapp extends Activity implements OnClickListener
     private ImageButton mFrdImg;
     private ImageButton mAddressImg;
 
+    private MyImgScroll myPager; // 图片容器
+    private LinearLayout ovalLayout; // 圆点容器
+    private List<View> listViews; // 图片组
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
@@ -52,24 +55,30 @@ public class Yneedapp extends Activity implements OnClickListener
         initView();
 
         initEvents();
+        myPager = (MyImgScroll) findViewById(R.id.mvp);
+        ovalLayout = (LinearLayout) findViewById(R.id.vb);
+        InitViewPager();//初始化图片
+        //开始滚动
+        myPager.start(this, listViews, 4000, ovalLayout,
+                R.layout.ad_botton_item, R.id.ad_item_v,
+                R.drawable.dot_focused, R.drawable.dot_normal);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void initEvents()
-    {
+    private void initEvents() {
         mTabWeixin.setOnClickListener(this);
         mTabFrd.setOnClickListener(this);
         mTabAddress.setOnClickListener(this);
-        mViewPager.addOnPageChangeListener(new OnPageChangeListener()
-        {
+        mViewPager.addOnPageChangeListener(new OnPageChangeListener() {
 
             @Override
-            public void onPageSelected(int arg0)
-            {
+            public void onPageSelected(int arg0) {
                 int currentItem = mViewPager.getCurrentItem();
                 resetImg();
-                switch (currentItem)
-                {
+                switch (currentItem) {
                     case 0:
                         mWeixinImg.setImageResource(R.drawable.tab_weixin_pressed);
                         break;
@@ -87,21 +96,18 @@ public class Yneedapp extends Activity implements OnClickListener
             }
 
             @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2)
-            {
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
 
             }
 
             @Override
-            public void onPageScrollStateChanged(int arg0)
-            {
+            public void onPageScrollStateChanged(int arg0) {
 
             }
         });
     }
 
-    private void initView()
-    {
+    private void initView() {
         mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
         // tabs
         mTabWeixin = (LinearLayout) findViewById(R.id.id_tab_weixin);
@@ -122,33 +128,28 @@ public class Yneedapp extends Activity implements OnClickListener
         mViews.add(tab02);
         mViews.add(tab03);
 
-        mAdapter = new PagerAdapter()
-        {
+        mAdapter = new PagerAdapter() {
 
             @Override
             public void destroyItem(ViewGroup container, int position,
-                                    Object object)
-            {
+                                    Object object) {
                 container.removeView(mViews.get(position));
             }
 
             @Override
-            public Object instantiateItem(ViewGroup container, int position)
-            {
+            public Object instantiateItem(ViewGroup container, int position) {
                 View view = mViews.get(position);
                 container.addView(view);
                 return view;
             }
 
             @Override
-            public boolean isViewFromObject(View arg0, Object arg1)
-            {
+            public boolean isViewFromObject(View arg0, Object arg1) {
                 return arg0 == arg1;
             }
 
             @Override
-            public int getCount()
-            {
+            public int getCount() {
                 return mViews.size();
             }
         };
@@ -158,11 +159,9 @@ public class Yneedapp extends Activity implements OnClickListener
     }
 
     @Override
-    public void onClick(View v)
-    {
+    public void onClick(View v) {
         resetImg();
-        switch (v.getId())
-        {
+        switch (v.getId()) {
             case R.id.id_tab_weixin:
                 mViewPager.setCurrentItem(0);
                 mWeixinImg.setImageResource(R.drawable.tab_weixin_pressed);
@@ -183,11 +182,120 @@ public class Yneedapp extends Activity implements OnClickListener
     /**
      * 将所有的图片切换为暗色的
      */
-    private void resetImg()
-    {
+    private void resetImg() {
         mWeixinImg.setImageResource(R.drawable.tab_weixin_normal);
         mFrdImg.setImageResource(R.drawable.tab_find_frd_normal);
         mAddressImg.setImageResource(R.drawable.tab_address_normal);
+    }
+
+    @Override
+    protected void onRestart() {
+        myPager.startTimer();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        myPager.stopTimer();
+        super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Yneedapp Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.yneed.yneed/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
+    }
+
+    private void InitViewPager() {
+        listViews = new ArrayList<View>();
+        int[] imageResId = new int[]{R.drawable.banner1, R.drawable.banner2,
+                R.drawable.banner3, R.drawable.d, R.drawable.banner4};
+        for (int i = 0; i < imageResId.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {// 设置图片点击事件
+                    Toast.makeText(Yneedapp.this,
+                            "点击了:" + myPager.getCurIndex(), Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+            imageView.setImageResource(imageResId[i]);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            listViews.add(imageView);
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Yneedapp Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.yneed.yneed/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+}
+
+
+
+
+
+
+// 滚动图适配器
+class  MyPagerAdapter  extends PagerAdapter {
+    private Activity mActivity; // 上下文
+    private List<View> mListViews; // 图片组
+    public MyPagerAdapter(){
+    }
+    public MyPagerAdapter(Activity mActivity,List<View> mListViews){
+        this.mActivity=mActivity;
+        this.mListViews=mListViews;
+    }
+    public int getCount() {
+        if (mListViews.size() == 1) {// 一张图片时不用流动
+            return mListViews.size();
+        }
+        return Integer.MAX_VALUE;
+    }
+    /**
+     返回List中的图片元素装载到控件中
+     */
+    public Object instantiateItem(View v, int i) {
+        if (((ViewPager) v).getChildCount() == mListViews.size()) {
+            ((ViewPager) v)
+                    .removeView(mListViews.get(i % mListViews.size()));
+        }
+        ((ViewPager) v).addView(mListViews.get(i % mListViews.size()), 0);
+        return mListViews.get(i % mListViews.size());
+    }
+
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return arg0 == (arg1);
+    }
+
+    public void destroyItem(ViewGroup view, int i, Object object) {
+        view.removeView(mListViews.get(i % mListViews.size()));
     }
 
 }
